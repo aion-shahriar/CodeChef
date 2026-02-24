d37890597def57ca9c23e0f873a6eb72d50c0cec
aion-shahriar <aionshahriar23@gmail.com>
2026-02-24 15:17:05 +0600

    REMMON.cpp for CodeChef problem solution
    1m

---
+++ Day-1/REMMON.cpp
@@ -0,0 +1,14 @@
+// https://www.codechef.com/problems/REMMON
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	
+	int n,a,b; cin>>n>>a>>b;
+	
+	cout<<n-(a*b)<<"\n";
+	
+	return 0;
+
+}
