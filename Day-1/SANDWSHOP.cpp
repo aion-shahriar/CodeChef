7b255fba1a165953ffdf6c856e31773695d8f042
aion-shahriar <aionshahriar23@gmail.com>
2026-02-24 15:24:48 +0600

    SANDWSHOP.cpp for CodeChef problem solution

---
+++ Day-1/SANDWSHOP.cpp
@@ -0,0 +1,14 @@
+// https://www.codechef.com/problems/SANDWSHOP
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	
+	int a,b,c; cin>>a>>b>>c;
+	
+	cout<<a-(b+c)<<"\n";
+	
+	return 0;
+
+}
