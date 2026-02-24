6733ab8b7200d99a85683043002a86f1f7ed6ce2
aion-shahriar <aionshahriar23@gmail.com>
2026-02-24 14:58:13 +0600

    Add initial implementation for NYRES problem
    Implement basic input and output for NYRES problem.

---
+++ Day-1/NYRES.cpp
@@ -0,0 +1,13 @@
+https://www.codechef.com/problems/NYRES
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int n; cin>>n;
+	cout<<n*31<<"\n";
+	
+	return 0;
+
+}
