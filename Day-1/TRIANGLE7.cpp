87fcb199b7f452a795c8e015ddfd67d364a8ce10
aion-shahriar <aionshahriar23@gmail.com>
2026-02-24 15:21:48 +0600

    TRIANGLE7.cpp to calculate third triangle angle
    Implement a program to calculate the third angle of a triangle.

---
+++ Day-1/TRIANGLE7.cpp
@@ -0,0 +1,14 @@
+// https://www.codechef.com/problems/TRIANGLE7
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	
+	int a,b; cin>>a>>b;
+	
+	cout<<180-(a+b)<<"\n";
+	
+	return 0;
+
+}
