635a6a03e7275bf713216605a7be1ae70b8805b0
aion-shahriar <aionshahriar23@gmail.com>
2026-02-24 15:29:50 +0600

    REDBLUEGEM.cpp for CodeChef problem solution
    Implement solution for the REDBLUEGEM problem from CodeChef.

---
+++ Day-1/REDBLUEGEM.cpp
@@ -0,0 +1,13 @@
+// https://www.codechef.com/problems/REDBLUEGEM
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	
+	int r,b,p,q; cin>>r>>b>>p>>q;
+	cout<<max(r*p,b*q);
+	
+	return 0;
+
+}
