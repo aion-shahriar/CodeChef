03936099471b380e19f9f7d8cc42d8b08e23d25b
aion-shahriar <aionshahriar23@gmail.com>
2026-02-25 12:47:07 +0600

    P1HOME.cpp to compare two integers
    Implement a program to check if two integers are equal.

---
+++ Day-2/P1HOME.cpp
@@ -0,0 +1,18 @@
+// https://www.codechef.com/problems/P1HOME
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	
+	int a,b; cin>>a>>b;
+	if(a==b) {
+	    cout<<"YES\n";
+	}
+	else {
+	    cout<<"NO\n";
+	}
+	
+	return 0;
+
+}
