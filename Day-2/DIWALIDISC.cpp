7b9c2e2f2b2e2384d0d67e5ae5d5de782421249b
aion-shahriar <aionshahriar23@gmail.com>
2026-02-25 13:46:45 +0600

    DIWALIDISC.cpp for CodeChef problem solution
    Implement solution for DIWALIDISC problem from CodeChef.

---
+++ Day-2/DIWALIDISC.cpp
@@ -0,0 +1,15 @@
+// https://www.codechef.com/problems/DIWALIDISC
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	
+	int a,b;
+	cin>>a>>b;
+	if(b>a) cout<<0<<"\n";
+	else cout<<a-b<<"\n";
+	
+	return 0;
+
+}
