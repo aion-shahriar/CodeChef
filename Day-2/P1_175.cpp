bd33bf54ef7155aa9fb09b1d2c31e44615db154c
aion-shahriar <aionshahriar23@gmail.com>
2026-02-25 14:23:41 +0600

    solution for CodeChef problem P1_175
    Implement solution for CodeChef problem P1_175.

---
+++ Day-2/P1_175.cpp
@@ -0,0 +1,14 @@
+// https://www.codechef.com/problems/P1_175
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	
+	int a,b;cin>>a>>b;
+	if(a<=b) cout<<"YES\n";
+	else cout<<"NO\n";
+	
+	return 0;
+
+}
