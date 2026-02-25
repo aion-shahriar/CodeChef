e472bb647ed6631a7537c3c48ae53199bf145d8f
aion-shahriar <aionshahriar23@gmail.com>
2026-02-25 14:37:42 +0600

    CHEFSOCKS.cpp for CodeChef problem solution
    Implement solution for CHEFSOCKS problem from CodeChef.

---
+++ Day-2/CHEFSOCKS.cpp
@@ -0,0 +1,14 @@
+// https://www.codechef.com/problems/CHEFSOCKS
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	
+	int a,b,c; cin>>a>>b>>c;
+	if(b+c>=a) cout<<"YES\n";
+	else cout<<"NO\n";
+	
+	return 0;
+
+}
