e1cd19a96b7fff374072ba3e5cd9ce58b874fc64
aion-shahriar <aionshahriar23@gmail.com>
2026-02-25 11:12:43 +0600

    EXUNPIZZA.cpp for CodeChef problem solution

---
+++ Day-2/EXUNPIZZA.cpp
@@ -0,0 +1,15 @@
+// https://www.codechef.com/problems/EXUNPIZZA
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	
+	int a,b,c;
+	cin>>a>>b>>c;
+	
+	cout<<(a-b)*c<<"\n";
+	
+	return 0;
+
+}
