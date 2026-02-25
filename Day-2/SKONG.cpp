8b8ccef4c7a4fbc056ea428feba6c04b11d93ad2
aion-shahriar <aionshahriar23@gmail.com>
2026-02-25 13:52:21 +0600

    SKONG.cpp for CodeChef problem solution
    Implement SKONG problem solution from CodeChef.

---
+++ Day-2/SKONG.cpp
@@ -0,0 +1,14 @@
+// https://www.codechef.com/problems/SKONG
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	
+	int x,h; cin>>x>>h;
+	cout<<(x-4)*24+h<<"\n";
+	
+	return 0;
+	
+
+}
