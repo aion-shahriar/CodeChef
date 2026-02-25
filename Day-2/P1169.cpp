3c788be288ef2f52981bad3ea678761ac36a19d1
aion-shahriar <aionshahriar23@gmail.com>
2026-02-25 13:23:54 +0600

    solution for CodeChef problem P1169
    Implement solution for CodeChef problem P1169.

---
+++ Day-2/P1169.cpp
@@ -0,0 +1,14 @@
+// https://www.codechef.com/problems/P1169
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	
+	int a; cin>>a;
+	if(a>=10) cout<<"YES\n";
+	else cout<<"NO\n";
+	
+	return 0;
+
+}
