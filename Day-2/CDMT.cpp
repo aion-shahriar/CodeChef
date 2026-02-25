e5d4e8b862c9f072be3e61eebddfec54ed390783
aion-shahriar <aionshahriar23@gmail.com>
2026-02-25 13:07:55 +0600

    CDMT.cpp for CodeChef problem solution
    Implement solution for the CDMT problem from CodeChef.

---
+++ Day-2/CDMT.cpp
@@ -0,0 +1,15 @@
+// https://www.codechef.com/problems/CDMT
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+	int a,b; cin>>a>>b;
+	
+	if(a>b) cout<<"NO\n";
+	else cout<<"YES\n";
+	
+	return 0;
+
+}
