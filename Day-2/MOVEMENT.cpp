a96d9a06cd72a97b8e3fca64b0cfef97b6418bc7
aion-shahriar <aionshahriar23@gmail.com>
2026-02-25 14:19:15 +0600

    MOVEMENT.cpp for CodeChef problem solution

---
+++ Day-2/MOVEMENT.cpp
@@ -0,0 +1,13 @@
+// https://www.codechef.com/problems/MOVEMENT
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	
+	int a,b,c,d; cin>>a>>b>>c>>d;
+	cout<<a-c<<" "<<b-d<<"\n";
+	
+	return 0;
+
+}
