611ea30ab73caf01f3578f76539d1159e6ded58a
aion-shahriar <aionshahriar23@gmail.com>
2026-02-25 11:09:16 +0600

    SHOM.cpp for CodeChef problem solution

---
+++ Day-2/SHOM.cpp
@@ -0,0 +1,13 @@
+// https://www.codechef.com/problems/SHOM
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	
+	int a,b; cin>>a>>b;
+	cout<<max(a,b)-min(a,b)<<"\n";
+	
+	return 0;
+
+}
