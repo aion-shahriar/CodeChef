dc54073592b4c169ea78571a54959d7c0eb91b1d
aion-shahriar <aionshahriar23@gmail.com>
2026-02-25 14:27:31 +0600

    PAYSC.cpp for CodeChef problem solution

---
+++ Day-2/PAYSC.cpp
@@ -0,0 +1,15 @@
+// https://www.codechef.com/problems/PAYSC
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	
+	int x; cin>>x;
+	int op=100+4*x;
+	
+	cout<<min(op,300)<<"\n";
+	
+	return 0;
+
+}
