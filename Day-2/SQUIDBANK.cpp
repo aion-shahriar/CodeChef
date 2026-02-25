6512dab4d5fe48daa72b27a9afc996aa72e2c1cd
aion-shahriar <aionshahriar23@gmail.com>
2026-02-25 14:43:51 +0600

    SQUIDBANK.cpp for CodeChef problem solution

---
+++ Day-2/SQUIDBANK.cpp
@@ -0,0 +1,13 @@
+// https://www.codechef.com/problems/SQUIDBANK
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	
+	int a,b; cin>>a>>b;
+	cout<<(a-b)*10000<<"\n";
+	
+	return 0;
+
+}
