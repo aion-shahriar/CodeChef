8e886765a7ab1c402c0969fa41ef7b29a5902025
aion-shahriar <aionshahriar23@gmail.com>
2026-02-25 13:27:39 +0600

    RECSQ.cpp for CodeChef problem solution
    Implement solution for the RECSQ problem from CodeChef.

---
+++ Day-2/RECSQ.cpp
@@ -0,0 +1,16 @@
+// https://www.codechef.com/problems/RECSQ
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	
+	int a,b,c;
+	cin>>a>>b>>c;
+	
+	if(a*b==c*c) cout<<"YES\n";
+	else cout<<"NO\n";
+	
+	return 0;
+
+}
