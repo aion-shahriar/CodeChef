27a314af595408f1d34fc48867ea853e9b780745
aion-shahriar <aionshahriar23@gmail.com>
2026-02-25 14:13:51 +0600

    FUELCHK.cpp for fuel check problem

---
+++ Day-2/FUELCHK.cpp
@@ -0,0 +1,14 @@
+// https://www.codechef.com/problems/FUELCHK
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	
+	int a,b; cin>>a>>b;
+	if(a*b>=100) cout<<"YES\n";
+	else cout<<"No\n";
+	
+	return 0;
+
+}
