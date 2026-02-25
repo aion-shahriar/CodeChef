1db30f9ad42eec326a66c421674bb90c2a486e69
aion-shahriar <aionshahriar23@gmail.com>
2026-02-25 12:44:37 +0600

    CRCK.cpp for CodeChef problem solution
    Implement solution for the CRCK problem from CodeChef.

---
+++ Day-2/CRCK.cpp
@@ -0,0 +1,13 @@
+// https://www.codechef.com/problems/CRCK
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int a; cin>>a;
+	cout<<25-a<<"\n";
+	
+	return 0;
+
+}
