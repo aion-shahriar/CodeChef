caedbac3a7d29ca675ed624bd647ac10d2a380d4
aion-shahriar <aionshahriar23@gmail.com>
2026-02-25 14:55:20 +0600

    LMP1.cpp for CodeChef problem solution
    Implement solution for LMP1 problem from CodeChef.

---
+++ Day-2/LMP1.cpp
@@ -0,0 +1,14 @@
+// https://www.codechef.com/problems/LMP1
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	
+	int x,y; cin>>x>>y;
+	if(x>=y) cout<<"YES\n";
+	else cout<<"NO\n";
+	
+	return 0;
+
+}
