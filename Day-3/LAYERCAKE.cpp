9ab335046adfb8539eda1613f1dbbf35aa548cc2
aion-shahriar <aionshahriar23@gmail.com>
2026-02-26 10:38:06 +0600

    875 rating.

---
+++ Day-3/LAYERCAKE.cpp
@@ -0,0 +1,28 @@
+
+// https://www.codechef.com/problems/LAYERCAKE
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	
+	int t; cin>>t;
+	while(t--) {
+	    int n,m; cin>>n>>m;
+	    vector<int>a(n), b(n);
+	    for(int i=0;i<n;i++) cin>>a[i];
+	    for(int i=0;i<m;i++) cin>>b[i];
+	    
+	    int cnt=0;
+	    for(int i=0;i<m;i++) {
+	        for(int j=0;j<n;j++) {
+	            if(a[j]>b[i]) {
+	                cnt++;
+	            }
+	        }
+	    }
+	    cout<<cnt<<"\n";
+	}
+	return 0;
+
+}
